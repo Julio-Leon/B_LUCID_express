fb49780148cb1cd89e2093a42cc54bc29e536184
julileon10 <julio.leon.diaz1@gmail.com>
2021-07-31 22:31:13 -0400

    server running

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = process.env.PORT || 4000
+
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+app.get('/', (req, res) => {
+    res.redirect('/')
+})
+
+app.listen(PORT, () => {
+    console.log('D-LUCID API is running on port:', PORT)
+})
